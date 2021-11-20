7d8e11998511534a9e0721f310cbaabdd31b4279
aidelojep <aidelojepeter123@gmail.com>
2021-11-20 18:12:28 +0100

    Hackerrank Question

--- src/com/hackerrank/questions/Solution.java
+++ src/com/hackerrank/questions/Solution.java
@@ -4,9 +4,15 @@ import java.io.BufferedWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MaximumOccuringCharacter {
+class MaximumOccuringCharacter {
 
         /*
+        QUESTION:
+        Given a string, return the character that appears the maximum number of times in the string,
+
+
+
+        HINTS:
          * Complete the 'maximumOccurringCharacter' function below.
          *
          * The function is expected to return a CHARACTER.
@@ -16,17 +22,19 @@ public class MaximumOccuringCharacter {
         public static char maximumOccurringCharacter(String text) {
             Map<Character, Integer>map = new HashMap<>();
             for(int i=0; i<text.length(); i++){
-                if(!map.containsKey(text.charAt(i))){
-                    map.put(text.charAt(i), 1);
+                if(map.containsKey(text.charAt(i))){
+                    map.replace(text.charAt(i), map.get(text.charAt(i))+1);
+
                 }
                 else{
-                    map.replace(text.charAt(i), map.get(text.charAt(i)+1));
+                    map.put(text.charAt(i), 1);
                 }
             }
             int max =0;
             for (Map.Entry<Character, Integer> map1: map.entrySet()){
-                if(map1.getValue().intValue() > max){
-                    max = map1.getValue().intValue();
+                System.out.println(map1.getKey() + " "+map1.getValue());
+                if(map1.getValue() > max){
+                    max = map1.getValue();
                 }
             }
             for (Map.Entry<Character, Integer> map1: map.entrySet()){
@@ -44,10 +52,8 @@ public class MaximumOccuringCharacter {
     public class Solution {
         public static void main(String[] args)  {
 
-
             char result = MaximumOccuringCharacter.maximumOccurringCharacter("helloWorld");
-
-           ;
+            System.out.println(result);
         }
     }
 
--- src/com/hackerrank/questions/StaticMethodCall.java
+++ src/com/hackerrank/questions/StaticMethodCall.java
@@ -1,2 +1,25 @@
-package com.hackerrank.questions;public class StaticMethodCall {
+package com.hackerrank.questions;
+
+public class StaticMethodCall {
+    public static int sum(int x, int y){
+        int sum = x + y;
+        return sum;
+    }
+
+    public static int sub(int x, int y){
+        int sub = x - y;
+        return sub;
+    }
+    public static int setSum(){
+
+        int setSum = sum(10,9) - sub(5,2);
+
+        return setSum;
+    }
+    public static void main(String[] args) {
+        System.out.println("The value of sum is: " + sum(4,5));
+        System.out.println("The value of sub is: " + sub(10, 9));
+        System.out.println("The value of the new setSum is: " + setSum());
+
+    }
 }
